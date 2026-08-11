e0ae0f8032730cf3764c3305495ba5a8b9f3154b
Thais <thaishdzlpz@gmail.com>
2026-08-11 17:17:41 +0100

    feat: implement Steam authentication using OpenID strategy

---
+++ server/src/config/passport.ts
@@ -0,0 +1,47 @@
+import passport, { DoneCallback } from 'passport';
+import { SteamOpenIdStrategy, SteamOpenIdUserProfile} from 'passport-steam-openid';
+import { SteamAuthService } from '../services/steam/SteamAuthService';
+import config from "./";
+
+const steamAuthService = new SteamAuthService()
+
+
+passport.use(
+    new SteamOpenIdStrategy({
+        returnURL: 'http://localhost:3000/auth/steam',
+        profile: true,
+        apiKey: config.API_KEY,
+        maxNonceTimeDelay: 30 // Optional, in seconds, time between creation and verification of nonce date
+    }, async (
+        req: Request,
+        identifier: string,
+        profile: SteamOpenIdUserProfile, // if profile is false, then it's only { steamid }, otherwise full profile from GetPlayerSummaries api
+        done: DoneCallback
+    ) => {
+        // Optional callback called only when successful authentication occurs
+        // You can save the user to database here.
+        try {
+            const user = await steamAuthService.findOrCreateSteamUser(profile.steamid, profile.personaname)
+            done(null, user) // llama a serializeUser
+        } catch (error) {
+            done(error as Error)
+        }
+    })
+)
+
+// Se ejecuta UNA VEZ, justo después del login y Passport guarda { _id: "abc123" } en la cookie de sesión
+passport.serializeUser((user: any, done) => {
+  done(null, user._id);
+});
+
+// Se ejecuta en CADA request que trae esa cookie, identifica quién es bajo esa cookie
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const { User } = await import('../models/User');
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
