220b1a377e8cde8dcbfd40521fee284c6c57025b
Thais <thaishdzlpz@gmail.com>
2026-08-11 17:16:58 +0100

    chore: clean up app.ts by removing redundant whitespace and organizing middleware usage

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ import routes from "./routes";
 import errorMiddleware from "./middlewares/error";
 
 const app = express();
-  
+
 // Configura un stream personalizado para que morgan envíe sus logs a Winston
 const stream = {
   write: (message: string) => logger.info(message.trim()), // Envía los logs de morgan a winston
@@ -15,14 +15,14 @@ const stream = {
 
 // Debe ir antes de la rutas para protegerlas
 app.use(cors()); // Permite TODAS las conexiones exteriores en PROD cambiarlo
+app.use(express.json()); // Middlware de entrada de datos,analiza el body para ver si es un JSON y lo parsea para el controller
+app.use(morgan("combined", { stream })); // Registra detalles de cada solicitud HTTP usando Winston
 
 // Routes
 app.use("/api/v1", routes);
 
-app.use(express.json()); // Middlware de entrada de datos,analiza el body para ver si es un JSON y lo parsea para el controller
-app.use(morgan("combined", { stream })); // Registra detalles de cada solicitud HTTP usando Winston
 
 // Global error handler (should be after routes)
-app.use(errorMiddleware); 
+app.use(errorMiddleware);
 
 export default app;
